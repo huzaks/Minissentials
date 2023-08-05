0ec84a3c0e39f31e9b21a665590fd1348dc9a7c2
huzaks <jeremylavalleegiroux@gmail.com>
2023-08-05 13:53:06 -0400

    first commit

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,18 @@
+'use client'
 import Image from 'next/image'
 import styles from './page.module.css'
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 export default function Home() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  };
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -90,6 +101,26 @@ export default function Home() {
           </p>
         </a>
       </div>
+      <Slider {...settings}>
+          <div>
+            <h3>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+          <div>
+            <h3>6</h3>
+          </div>
+        </Slider>
     </main>
   )
 }
